8698a021b56747d759ce77f92898ec7bdd399a02
JcJet <jcjet88@gmail.com>
2023-05-17 01:38:26 +0300

    ms-compatibility

--- src/files.controller.ts
+++ src/files.controller.ts
@@ -9,13 +9,13 @@ import { ApiOperation, ApiResponse } from "@nestjs/swagger";
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
-  @MessagePattern('createFile')
+  @MessagePattern({ cmd: 'createFile' })
   async createFile(
     @Payload() data: { file: any; dto: FileDto },
   ): Promise<string> {
     return await this.filesService.createFile(data.file, data.dto);
   }
-  @MessagePattern('deleteFileByName')
+  @MessagePattern({ cmd: 'deleteFileByName' })
   async deleteFile(@Payload() data: { fileName: string }): Promise<any> {
     return await this.filesService.deleteFile(data.fileName);
   }
@@ -27,12 +27,12 @@ export class FilesController {
     return await this.filesService.editFile(data.id, data.dto);
   }*/
 
-  @MessagePattern('getFiles')
+  @MessagePattern({ cmd: 'getFiles' })
   async getFiles(@Payload() data: { dto: FileDto }): Promise<FileRecord[]> {
     return await this.filesService.getFiles(data.dto);
   }
 
-  @MessagePattern('deleteFiles')
+  @MessagePattern({ cmd: 'deleteFiles' })
   async deleteFiles(@Payload() data: { dto: FileDto }): Promise<FileRecord[]> {
     return await this.filesService.deleteFiles(data.dto);
   }
